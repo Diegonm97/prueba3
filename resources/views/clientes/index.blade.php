40275f83123dc09a2c3c9be4cb53aafca8d9a721
Diego Nuñez <diego.nm97@hotmail.com>
2018-10-12 17:02:44 -0500

    primer

---
+++ resources/views/clientes/index.blade.php
@@ -0,0 +1,91 @@
+@extends('layouts.apphome')
+@section('content')
+
+
+<p>
+    
+    <h1 align="center">Informacion Clientes</h1>
+</p>
+
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="header">
+                                <h4 class="title">Clientes</h4>
+                                <p class="category">Aqui se muestran datos de los clientes registrados</p>
+                            </div>
+                            <div class="content table-responsive table-full-width">
+                                <table class="table table-hover table-striped">
+                                    <thead>
+                                          	<th>Nombre</th>
+                                        	<th>Estado</th>
+                                        	<th>Tipo Cliente</th>
+                                        	<th>Direccion</th>
+                                        	<th>Ciudad</th>
+                                        	<th>Telefono</th>
+                                        	<th>Email</th>
+                                        	<th>Fecha Nac.</th>
+                                        	<th>Salario</th>
+                                        	<th>Rango</th>
+                                        	<th>EPS</th>
+                                        	<th>ARL</th>
+                                        	<th>Caja Comp.</th>
+                                        	<th>Beneficio</th>
+                                        	<th>Fecha Ing.</th>
+                                        	<th>NIT emp.</th>
+                                        	<th>Nombre emp.</th>
+                                        	<th>UPC adic.</th>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                        	<td>Nombre</td>
+                                        	<td>Estado</td>
+                                        	<td>Tipo Cliente</td>
+                                        	<td>Direccion</td>
+                                        	<td>Ciudad</td>
+                                        	<td>Telefono</td>
+                                        	<td>Email</td>
+                                        	<td>Fecha Nac.</td>
+                                        	<td>Salario</td>
+                                        	<td>Rango</td>
+                                        	<td>EPS</td>
+                                        	<td>ARL</td>
+                                        	<td>Caja Comp.</td>
+                                        	<td>Beneficio</td>
+                                        	<td>Fecha Ing.</td>
+                                        	<td>NIT emp.</td>
+                                        	<td>Nombre emp.</td>
+                                        	<td>UPC adic.</td>
+                                        </tr>
+                                        <tr>
+                                        	<td>Nombre</td>
+                                        	<td>Estado</td>
+                                        	<td>Tipo Cliente</td>
+                                        	<td>Direccion</td>
+                                        	<td>Ciudad</td>
+                                        	<td>Telefono</td>
+                                        	<td>Email</td>
+                                        	<td>Fecha Nac.</td>
+                                        	<td>Salario</td>
+                                        	<td>Rango</td>
+                                        	<td>EPS</td>
+                                        	<td>ARL</td>
+                                        	<td>Caja Comp.</td>
+                                        	<td>Beneficio</td>
+                                        	<td>Fecha Ing.</td>
+                                        	<td>NIT emp.</td>
+                                        	<td>Nombre emp.</td>
+                                        	<td>UPC adic.</td>
+                                        </tr>
+                                        
+                                        
+                                    </tbody>
+                                </table>
+
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+@endsection
---
+++ resources/views/layouts/apphome.blade.php
@@ -0,0 +1,211 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <link rel="shortcut icon" href="/assets/img/intersaludfavicon.ico">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+
+  <title>Intersalud del Valle</title>
+
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+
+
+    <!-- Bootstrap core CSS     -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- Animation library for notifications   -->
+    <link href="assets/css/animate.min.css" rel="stylesheet"/>
+
+    <!--  Light Bootstrap Table core CSS    -->
+    <link href="assets/css/light-bootstrap-dashboard.css?v=1.4.0" rel="stylesheet"/>
+
+    <!--     Fonts and icons     -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+    <link href="assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
+    
+
+</head>
+<body>
+
+<div class="wrapper">
+    <div class="sidebar" data-color="blue" data-image="assets/img/sidebar-4.jpg">
+
+    <!--
+
+        Tip 1: you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple"
+        Tip 2: you can also add an image using data-image tag
+
+    -->
+
+      <div class="sidebar-wrapper">
+            <div class="logo">
+                <a href="home" class="simple-text">
+                    Intersalud del Valle
+                </a>
+            </div>
+
+            <ul class="nav">
+                <li>
+                    <a href="clientes">
+                        <i class="pe-7s-user"></i>
+                        <p>Clientes</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <i class="pe-7s-note2"></i>
+                        <p>Configuracion</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <i class="pe-7s-news-paper"></i>
+                        <p>Pagos</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <i class="pe-7s-portfolio"></i>
+                        <p>Empresas</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <i class="pe-7s-map-marker"></i>
+                        <p>Sedes</p>
+                    </a>
+                </li>
+                
+        
+
+            </ul>
+      </div>
+    </div>
+
+    <div class="main-panel">
+        <nav class="navbar navbar-default navbar-fixed">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Menu</a>
+                </div>
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-left">
+                        <li>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="fa fa-dashboard"></i>
+                            </a>
+                        </li>
+                    </ul>
+
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                           <a href="">
+                               Cuenta
+                            </a>
+
+                            @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                @if (Route::has('register'))
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                @endif
+                            </li>
+                        @else
+                            <li class="dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu">
+                                  <li>
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Salir del sistema') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                  </li> 
+                                </ul>
+                            </li>
+                        @endguest
+                        </li>
+                    </ul>
+
+
+
+
+                </div>
+            </div>
+        </nav>
+
+
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+
+                  @yield('content')
+
+                </div>
+            </div>
+        </div>
+
+
+        <footer class="footer">
+            <div class="container-fluid">
+                <nav class="pull-left">
+                    <ul>
+                        <li>
+                            <a href="home">
+                                Pag. Principal
+                            </a>
+                        </li>
+
+                    </ul>
+                </nav>
+                <p class="copyright pull-right">
+                    &copy; <script>document.write(new Date().getFullYear())</script> <a href="">Intersalud del Valle</a>
+                </p>
+            </div>
+        </footer>
+
+    </div>
+</div>
+
+
+</body>
+
+    <!--   Core JS Files   -->
+    <script src="assets/js/jquery.3.2.1.min.js" type="text/javascript"></script>
+  <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+
+  <!--  Charts Plugin -->
+  <script src="assets/js/chartist.min.js"></script>
+
+    <!--  Notifications Plugin    -->
+    <script src="assets/js/bootstrap-notify.js"></script>
+
+    <!--  Google Maps Plugin    -->
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+
+    <!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
+  <script src="assets/js/light-bootstrap-dashboard.js?v=1.4.0"></script>
+
+  <!-- Light Bootstrap Table DEMO methods, don't include it in your project! -->
+  <script src="assets/js/demo.js"></script>
+
+
+</html>
