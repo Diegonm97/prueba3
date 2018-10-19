c7aea71ac61e53752edb0c72280b47c4c9f93dfd
Diego Nuñez <diego.nm97@hotmail.com>
2018-10-19 18:02:33 -0500

    Segundo commit

--- app/Clientes.php
+++ app/Clientes.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Clientes extends Model
 {
-    //
+    protected $table = 'clientes';
+
+     protected $fillable=['nuip','nombreEmpresaContraCli','nombreCli','tipoIdCli','direccionCli','oficinaCli','ciudadCli','telefonoCli','emailCli','fechaNacCli','salarioCli','rangoCli','nombreEpsCli','ARLcli','AFPcli','EPScli','cajaCompCli','beneficioCli','fechaIngresoCli','idEmpresaContraCli','idNIT','UPCadicCli','estadoPago'];
 }
