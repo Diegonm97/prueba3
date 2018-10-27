66a83d08dee67d2911875045796c609ffa3a0e3a
Diego Nuñez <diego.nm97@hotmail.com>
2018-10-27 17:11:21 -0500

    Show terminado

--- resources/views/clientes/show.blade.php
+++ resources/views/clientes/show.blade.php
@@ -7,12 +7,28 @@
     <h1 align="center">Informacion Clientes</h1>
 </p>
 
- <p>{{$cliente -> nombreCli}}
-                        {{$cliente->estadoCli}}
-                                    {{$cliente->tipoIdCli}}
-                                    {{$cliente->direccionCli}}
-                                    {{$cliente->oficinaCli}}
-                                    {{$cliente->ciudadCli}}
+ <p>				
+ 	<strong>Nombre: </strong>{{$cliente->nombreCli}}<br/>
+    <strong>Estado: </strong>{{$cliente->estadoCli}}<br/>
+    <strong>Tipo: </strong>{{$cliente->tipoIdCli}}<br/> 
+    <strong>Direccion: </strong>{{$cliente->direccionCli}} <br/>
+    <strong>Oficina: </strong>{{$cliente->oficinaCli}} <br/>
+    <strong>Ciudad: </strong>{{$cliente->ciudadCli}} <br/>
+    <strong>Telefono: </strong>{{$cliente->telefonoCli}} <br/>
+    <strong>Email: </strong>{{$cliente->emailCli}} <br/>
+    <strong>Fecha de Nacimiento: </strong>{{$cliente->fechaNacCli}} <br/>
+    <strong>Salario: </strong>{{$cliente->salarioCli}} <br/>
+    <strong>Rango: </strong>{{$cliente->rangoCli}} <br/>
+    <strong>EPS: </strong>{{$cliente->EPScli}} <br/>
+    <strong>ARL: </strong>{{$cliente->ARLcli}} <br/>
+    <strong>AFP: </strong>{{$cliente->AFPcli}} <br/>
+    <strong>Caja de Compensacion: </strong>{{$cliente->cajaCompCli}} <br/>
+    <strong>Beneficio: </strong>{{$cliente->beneficioCli}} <br/>
+    <strong>Fecha de Ingreso: </strong>{{$cliente->fechaIngCli}} <br/>
+    <strong>Empresa Contratante: </strong>{{$cliente->idEmpresaContraCli}}-{{$cliente->idNIT}}  <br/>
+    <strong>Nombre Empresa: </strong>{{$cliente->nombreEmpCli}} <br/>
+    <strong>UPC Adicional: </strong>{{$cliente->UPCadicCli}} <br/>
+    <strong>Estado: </strong>{{$cliente->estadoPago}} <br/>
 </p>
 
 
