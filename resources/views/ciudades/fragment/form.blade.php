ef612cfc4c4add38f823b99901377c10d199ec22
Diego Nuñez <diego.nm97@hotmail.com>
2019-03-25 17:00:42 -0500

    cambios

--- resources/views/ciudades/fragment/form.blade.php
+++ resources/views/ciudades/fragment/form.blade.php
@@ -1,23 +1,22 @@
 <div class="col-sm-12">
 
-<div class="col-sm-3">
-<div class="form-group row has-success">
-{!! Form::label('nombre','Nombre de la ciudad(*).')!!}
-{!!Form::text('nombre',null,['class'=>'form-control','title'=>'Ingresa el nombre de la ciudad.' , 'placeholder'=>'Ej: Confandi', 'onKeyUp'=>'this.value = this.value.toUpperCase()','id'=>'nombre','required'=>'required' ])!!}
-</div>
-</div>
+    <div class="col-sm-3">
+        <div class="form-group row has-success">
+            {!! Form::label('nombre','Nombre de la ciudad(*).')!!}
+            {!!Form::text('nombre',null,['class'=>'form-control','title'=>'Ingresa el nombre de la ciudad.' , 'placeholder'=>'Ej: Confandi', 'onKeyUp'=>'this.value = this.value.toUpperCase()','id'=>'nombre','required'=>'required' ])!!}
+        </div>
+    </div>
 
-<div class="col-sm-3">
-<div class="form-group row has-success">
-{!! Form::label('codigo','Codigo de la ciudad(*).')!!}
-{!!Form::number('codigo',null,['class'=>'form-control','title'=>'Ingresa el nombre de la ciudad.' , 'placeholder'=>'Ej: 123','id'=>'codigo','required'=>'required' ])!!}
-</div>
-</div>
+    <div class="col-sm-3">
+        <div class="form-group row has-success">
+            {!! Form::label('codigo','Codigo de la ciudad(*).')!!}
+            {!!Form::number('codigo',null,['class'=>'form-control','title'=>'Ingresa el nombre de la ciudad.' , 'placeholder'=>'Ej: 123','id'=>'codigo','required'=>'required' ])!!}
+        </div>
+    </div>
 
 </div>
 <br>
 <br>
 <div class="form-group text-center">
-{!!Form::button('<i class="fa fa-floppy-o" aria-hidden="true"></i>', array('type' => 'submit', 'class'=>'btn btn-primary btn-lg btn-block'))!!}
+    {!!Form::button('<i class="fa fa-floppy-o" aria-hidden="true"></i>', array('type' => 'submit', 'class'=>'btn btn-primary btn-lg btn-block'))!!}
 </div> 
-
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Intersalud del Valle</title>
+    <title>Intersalud Colombia</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
