ae6e98ca9099499ce06e212fb93f182a1aadca2b
Diana Caro <carito.417@hotmail.com>
2019-04-05 11:09:51 -0500

    cambios en home

--- resources/views/empresas/show.blade.php
+++ resources/views/empresas/show.blade.php
@@ -140,50 +140,5 @@
 </div>
 
 
-
-<div class="col-md-12">
-    <h2 align="center">Empleados</h2>
-    <div class="content table-responsive table-full-width">
-        <table class="table table-hover table-striped">
-            <div class="container">
-                <div class="row">
-                    <thead>
-                        <th>nuip</th>
-                        <th>Nombre</th>
-                        <th>Estado</th>
-                        <th>Tipo Cliente</th>
-                        <th>Telefono</th>
-                        <th>Rango</th>
-                        <th>Accion</th>
-
-                    </thead>
-                    <tbody>
-                        @foreach ($empleado_empresas as $empleado_empresa)
-                        <tr>
-                            <th>{{$empleado_empresa->identificacion}}</th>
-                            <th>{{$empleado_empresa->nombres}}</th>
-                            <th>{{$empleado_empresa->apellidos}}</th>
-                            <th>{{$empleado_empresa->estado}}</th>
-                            <th>{{$empleado_empresa->telefono}}</th>
-                            <th>{{$empleado_empresa->email}}</th>
-                            <th>
-
-                                <a href="{{route('empleado_empresa.show', $empleado_empresa->id)}}"><i class="far fa-eye"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
-                                <a href="{{route('empleado_empresa.edit', $empleado_empresa->id)}}"><i class="fas fa-pen"></i></a>
-
-                            </th>
-
-                        </tr>
-                        @endforeach
-
-                    </tbody>
-                </div>
-            </div>
-        </table>
-        <div align="center">
-            {!!$empleado_empresas->render() !!}
-        </div>
-    </div>
-</div>
+<!--  -->
 @endsection 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,20 +3,48 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card">
-                <div class="card-header">Escritorio</div>
 
-                <div class="card-body">
+                <div class="card-body" style="text-align: center;">
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
                         {{ session('status') }}
                     </div>
                     @endif
 
-                    Has ingresado al sistema! </div>
+                    <h4 > Bienvenido a <b style="color: #3472F7">Intersalud del Valle</b></h4> </div>
+
+                    
             </div>
         </div>
+
     </div>
+
+    @role('administrador')
+    <div class="row">
+        <div class="col-md-1"></div>
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <div class="caption" style="text-align: center">
+            <h3>Ingresos mes {{$mes}}</h3>
+            <h3> ${{number_format($total,0,',','.')}}</h3>
+            <!-- <p><a href="#" class="btn btn-primary" role="button">Button</a></p> -->
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <div class="caption" style="text-align: center">
+            <h3>Ingresos del día ({{$dia}}/{{$mes}})</h3>
+            <h3> ${{number_format($totald,0,',','.')}}</h3>
+            <!-- <p><a href="#" class="btn btn-primary" role="button">Button</a></p> -->
+          </div>
+        </div>
+      </div>
+      <div class="col-md-2"></div>
+    </div>
+    @endrole
 </div>
 @endsection 
